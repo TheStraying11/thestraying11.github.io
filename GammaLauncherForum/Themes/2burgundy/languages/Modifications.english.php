03f5783b351473b42d7ace906b07db8d3094cb84
TheStraying11 <jtrycroft@gmail.com>
2020-05-15 02:35:07 +0100

    im lazy

--- GammaLauncherForum/Themes/2burgundy/languages/Modifications.english.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Version: 2.0 Beta 4; Modifications
-
-// Important! Before editing these language files please read the text at the top of index.english.php.
-
-?>
--- GammaLauncherForum/cache/data_233c0777930a80008bb6c29c3049896f-SMF-user_last_visit-1.php
+++
@@ -1,1 +0,0 @@
-<?php if (!defined('SMF')) die; if (1589523307 < time()) $expired = true; else{$expired = false; $value = 's:1:"6";';}?>
