e37a52fa1035313c7fcdd7df64ff247e9ba1dff8
TheStraying11 <jtrycroft@gmail.com>
2020-05-15 02:26:36 +0100

    im lazy

---
+++ GammaLauncherForum/Themes/2burgundy/languages/Settings.english.php
@@ -0,0 +1,9 @@
+<?php
+// Version: 2.0 Beta 4 Public; Settings
+
+// Important! Before editing these language files please read the text at the topic of index.english.php.
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
+$txt['theme_description'] = 'Dark burgundy theme..<br /><br />Author: <i><a href="http://www.mustangv8.com">Thierry Chabry</a></i>.';
+
+?>
---
+++ GammaLauncherForum/cache/data_233c0777930a80008bb6c29c3049896f-SMF-user_last_visit-1.php
@@ -0,0 +1,1 @@
+<?php if (!defined('SMF')) die; if (1589523307 < time()) $expired = true; else{$expired = false; $value = 's:1:"6";';}?>
